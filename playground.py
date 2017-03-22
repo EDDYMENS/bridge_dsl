a7dddea8c3ed0f987047615b4ba5d5d081a159ff
eddymens <kwasiamantin@gmail.com>
2017-03-22 07:46:50 +0000

    add comments

--- playground.py
+++ playground.py
@@ -5,6 +5,8 @@ company_name = 'interflix'
 
 rules = Rules();
 
+#sample DSL usage
+#EG. Check if account type is enterprise and also if accout is premium only then allow a discount of 3 on charge rates
 rules. \
     on_query().whenever(business_type == "enterprise").run('Accounts', 'account_type', [company_name]) \
     .whenever(rules.results == "premium")\
